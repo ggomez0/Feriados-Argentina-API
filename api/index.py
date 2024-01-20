f56def5ccec24e1075761f4b7b50fa6ac84292d1
Gaspar Gomez <77340598+ggomez0@users.noreply.github.com>
2024-01-19 22:43:54 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,11 +6,6 @@ import json
 
 app = Flask(__name__)
 CORS(app)
-cors = CORS(app, resource={
-    r"/*":{
-        "origins":"*"
-    }
-})
 
 # Obtener la ruta del directorio actual
 current_directory = os.path.dirname(os.path.realpath(__file__))
